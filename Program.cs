fcf1f2ed6b1a7c11d1a89d2a68a94581894348ed
Edythator <edythator@gmail.com>
2019-11-11 23:28:31 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿//What are you doing here?
+using System;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace md5Checker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "";
+
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Which file would you like to calculate the MD5/SHA1/CRC32 hash of?");
+                path = Console.ReadLine();
+            }
+
+            else
+            {
+                path = args[0];
+            }
+
+            byte[] input = File.ReadAllBytes(path);
+
+            MD5 md5 = MD5.Create();
+            SHA1 sha1 = SHA1.Create();
+
+            Console.WriteLine("\nMD5: " + BitConverter.ToString(md5.ComputeHash(input)).Replace("-", "") 
+                + "\nSHA1: " + BitConverter.ToString(sha1.ComputeHash(input)).Replace("-", "") 
+                + "\nCRC32: " + Crc32.Compute(input));
+
+            Console.ReadLine();
+        }
+    }
+}
